027abc10fa2d814ac726b6eea9c96c374e0aaa21
nabilaaisa72 <162269708+nabilaaisa72@users.noreply.github.com>
2024-06-10 18:37:51 +0700

    backend-5

---
+++ server2.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const routerMhs = require('./routers/mahasiswa')
+const routerMk = require('./routers/matakuliah')
+const routerNilai = require('./routers/nilai')
+const app = express();
+const port = 3307;
+const cors = require('cors')
+
+// Untuk menerima req.body
+app.use(express.json());
+app.use(routerMhs)
+app.use(routerMk)
+app.use(routerNilai)
+app.use(express.urlencoded({ extended: true }));
+app.use(cors({
+    origin: '*'
+}))
+
+
+app.listen(port, () => {
+    console.log(`Server berjalan pada localhost:${port}`);
+});
